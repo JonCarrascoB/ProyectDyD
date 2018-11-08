c8e99448dd61ca33a980ca13d63d4d973017b170
Jon Carrasco <jcarrascobar@gmail.com>
2018-11-08 18:00:21 +0100

    commit9

--- DyD/app.js
+++ DyD/app.js
@@ -1,44 +1,94 @@
-function arranca(data) {
+//(function init() {
+//    let option = document.getElementById("actualizar_btn");
+//    if (actualizarBtn !== null) {
+//        actualizarBtn.addEventListener("click", obtenerDatosCarta);
+//    }
 
+//    let nombreCarta = localStorage.getItem("carta");
+//    if (nombreCarta !== null) {
+
+//        //document.getElementById("cartas").innerHTML = nombreCarta;
+
+//    } else {
+
+//        obtenerDatosCarta();
+
+//    }
+
+//}());
+
+let option;
+(function ini() {
+    option = document.getElementById("first_menu").value;
+    if (first_menu !== null) {
+        first_menu.addEventListener("change", selectPart);
+    }
+} ());
+
+
+function selectPart() {
     let xhr = new XMLHttpRequest();
-    let url = "https://api.nasa.gov/planetary/apod?api_key=2uaBh52XsqEIyC5HRTv9BWTyV3TYqH8vTQvYPgvV&date=" + data;
+    let url = "http://dnd5eapi.co/api/" + option;
     xhr.open("GET", url);
-    //xhr.onreadystatechange = function () {
-    //    if (xhr.readyState === 4 && xhr.status === 200) {
-    //        let responseObject = JSON.parse(xhr.response);
-    //        showDate(responseObject.date, responseObject.explanation, responseObject.title, responseObject.url);
-    //    }
-    //    else if (xhr.readyState === 4 && xhr.status === 400) {
-    //        document.getElementById("info").innerHTML = "fecha incorrecta";
-    //    }
-    //};
-
-    xhr.addEventListener("load", function () {
-        let responseObject = JSON.parse(xhr.response);
-        showDate(responseObject.date, responseObject.explanation, responseObject.title, responseObject.url);
-    });
-    xhr.addEventListener("error", function () {
-        console.log("error");
-    });
-
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let dates = JSON.parse(xhr.response).results;
+            console.log(dates.name);
+            //showDates(dates);
+        }
+        else if (xhr.readyState === 4 && xhr.status === 400) {
+            console.log("Error en el servidor");
+        }
+    };
     xhr.send();
+}
+
+function showDates() {
 
 }
 
-function showDate(date, explanation, title, image_url) {
-    let result = "<p>" + date + "</p>";
-    result += "<p>" + explanation + "</p>";
-    result += "<p>" + title + "</p>";
-    result += "<img src='" + image_url + "'/>";
-    document.getElementById("info").innerHTML = result;
-}
 
-function crearF() {
-    //let year = document.getElementById("year").value;
-    //let month = document.getElementById("month").value;
-    //let day = document.getElementById("day").value;
-    let data;
-    data = document.getElementById("fecha").value;
-    //document.getElementById("date").innerHTML = data;
-    arranca(data);
+
+function mostrarDatos(cartas) {
+
+    let nombreCartas = [];
+
+    for (let i = 0; i < cartas.cards.length; i++) {
+
+        nombreCartas.push(cartas.cards[i].name);
+
+    }
+
+    localStorage.setItem("cartas", JSON.stringify(nombreCartas));
+
+    // se pueden usar removeItem y clearItem para borrar elementos guardados
+
+
+
+    //let result = "";
+
+    for (let i = 0; i < nombreCartas.length; i++) {
+
+        let texto = document.createTextNode(nombreCartas[i]);
+
+        let parrafo = document.createElement("li");
+
+        parrafo.setAttribute("class", "carta col-sm-3 list-group-item");
+
+        parrafo.setAttribute("onclick", "alert('" + nombreCartas[i] + "')");
+
+        parrafo.appendChild(texto);
+
+        document.getElementById("fila_cartas").appendChild(parrafo);
+
+        // para eliminar, usar elementoHTML.removeChild(elementoHTML)
+
+
+
+        //result += "<p class='carta'>" + nombreCartas[i] + "</p>";
+
+    }
+
+    //document.getElementById("cartas").innerHTML = result;
+
 }
